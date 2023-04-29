1966633ddca8eedc7da60bf1508a0821b1d1259c
MariusSjo <marius.sjoberg@gmail.com>
2023-04-29 18:27:06 +0200

    remove supportfile

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
