850c3a6a4101cd288dfc0a4e2eaf0064c238d6b9
Marius Christopher Sjøberg <marius-christopher.sjoberg@gjensidige.no>
2026-01-15 21:37:09 +0100

    fix: remove commented code and unused Sentry integration (#24 #29)
    - Removed all commented code from next.config.js (Sentry config)
    - Removed commented code from pages/api/contact.js (email credentials)
    - Deleted unused sentry-example-page.js
    - Removed @sentry/nextjs dependency from package.json
    - Cleaned up contact API file with placeholder comments
    
    No credentials were ever committed to git history (verified).
    Addresses security issue #24 and code quality issue #29.

--- next.config.js
+++ next.config.js
@@ -1,51 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-
-
 module.exports = {
-  /* async redirects() {
-    return [
-      {
-        source: '/:slug',
-        destination: '/:slug', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ]
-  }, */
   reactStrictMode: false,
   trailingSlash: true,
 }
-
-// Inected Content via Sentry Wizard Below
-
-/* const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-
-    org: "fysi",
-    project: "javascript-nextjs",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-); */
